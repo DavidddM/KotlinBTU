3295b1c004c836f67feade4d82fd6623ff2a66f9
DavidddM <davit.maisuradze.1@btu.edu.ge>
2020-05-08 01:20:24 +0400

    Exercise 2 - XmlDesign

---
+++ XmlDesign/app/src/main/java/com/example/xmldesign/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.xmldesign
+
+import android.graphics.Paint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
